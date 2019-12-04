eeacb9a874eb508ba2c07576a2563a588f227287
Peter Garrow <garrow.peter@gmail.com>
2019-12-04 16:16:20 -0700

    Switch to theme-ui components, add color to links

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,3 +1,5 @@
+import { lighten } from "@theme-ui/color"
+
 export default {
   space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
   fonts: {
@@ -19,6 +21,7 @@ export default {
     border: "#DDD",
     primary: "#eb7b15",
     secondary: "#e4ab15",
+    accent: "#F8C99E",
   },
   button: {
     link: {
@@ -28,8 +31,31 @@ export default {
       py: `2`,
       transition: `0.3s all`,
       borderRadius: `1`,
+      backgroundColor: lighten(`primary`, 0.25),
+      borderWidth: `1px`,
+      borderColor: `primary`,
+      borderStyle: `solid`,
+      color: `text`,
+      boxShadow: `0px 0px 4px 0px #a4a4a4`,
       "&:hover": {
-        backgroundColor: `border`,
+        backgroundColor: lighten(`primary`, 0.1),
+      },
+    },
+  },
+  layout: {
+    container: {
+      maxWidth: `960px`,
+      header: {
+        display: `flex`,
+        flexDirection: `row`,
+        justifyContent: `space-between`,
+        py: `4`,
+      },
+      footer: {
+        display: `flex`,
+        flexDirection: `row`,
+        justifyContent: `space-between`,
+        py: `4`,
       },
     },
   },
